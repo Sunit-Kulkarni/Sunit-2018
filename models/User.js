0d06ceba19f363cc22009c7e3fbdd65778ccd3cd
Sunit Kulkarni <sunit.kulkarni@protonmail.com>
2018-07-12 17:46:12 -0400

    replicating other authentication flow from Emaily

--- models/User.js
+++ models/User.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  googleId: String,
-  credits: { type: Number, default: 0 }
+  pgpPublicKey: { type: String, default: '' },
+  message: { type: String, default: '' }
 });
 
 mongoose.model('users', userSchema);
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,6 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
-const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
@@ -16,22 +15,18 @@ passport.deserializeUser((id, done) => {
 });
 
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-      proxy: true
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      const existingUser = await User.findOne({ googleId: profile.id });
-
-      if (existingUser) {
-        return done(null, existingUser);
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
       }
-
-      const user = await new User({ googleId: profile.id }).save();
-      done(null, user);
-    }
-  )
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  })
 );
