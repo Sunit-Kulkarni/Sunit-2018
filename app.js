ba9c8067ef9ca53b94291acd8476c58938f8e7c3
Sunit Kulkarni <sunit.kulkarni@protonmail.com>
2018-07-12 16:49:32 -0400

    adding models, routes, and services folders for segmenting parts of the authentication flow. This is borrowed from the Emaily App

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var openpgp = require('openpgp');
-var app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+const openpgp = require('openpgp');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
